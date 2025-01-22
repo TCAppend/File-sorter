52f3b035066649ad6c28bb511696e216cab39cd2
TCJay_Append <123647533+TCBJYT@users.noreply.github.com>
2025-01-23 05:18:00 +0800

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,14 +1,15 @@
 import os, shutil
 
-pathinput = input('Input file: ')
+pathinput1 = input('Input file directory: ')
 
-path = (r"{pathinput}")
+
+path = pathinput1 + '/'
 
 file_name = os.listdir(path)
 
-folder_names = ['PNG files', 'MP4 files', 'MP3 files']
+folder_names = ['IMAGE files', 'MP4 files', 'MP3 files', 'GIF files']
 
-for loop in range(0,3):
+for loop in range(0,4):
     if not os.path.exists(path + folder_names[loop]):
         print(path + folder_names[loop])
         os.makedirs((path + folder_names[loop]))
@@ -17,10 +18,17 @@ for loop in range(0,3):
 for file in file_name:
     if ".mp3" in file and not os.path.exists(path + "/MP3 files/" +  file):
         shutil.move(path + file, path + "/MP3 files/" +  file)
-    if ".png" in file and not os.path.exists(path + "/PNG files/" + file):
-        shutil.move(path + file, path + "/PNG files/" +  file)
+    if ".png" in file and not os.path.exists(path + "/IMAGE files/" + file):
+        shutil.move(path + file, path + "/IMAGE files/" +  file)
     if ".mp4" in file and not os.path.exists(path + "/MP4 files/" + file):
         shutil.move(path + file, path + "/MP4 files/" +  file)
+    if ".gif" in file and not os.path.exists(path + "/GIF files/" + file):
+        shutil.move(path + file, path + "/GIF files/" +  file)
+    if ".jpeg" in file and not os.path.exists(path + "/IMAGE files/" + file):
+        shutil.move(path + file, path + "/IMAGE files/" +  file)
+    if ".jpg" in file and not os.path.exists(path + "/IMAGE files/" + file):
+        shutil.move(path + file, path + "/IMAGE files/" +  file)
+    
 
 else:
     print("Some files didn't move")
