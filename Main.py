59a3e617d291345f3446dbd13809453e5320d609
TCJay_Append <123647533+TCBJYT@users.noreply.github.com>
2025-01-26 19:23:07 +0800

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,34 +1,43 @@
 import os, shutil
 
-pathinput1 = input('Input file directory: ')
+folder_names = ['IMAGE files', 'VIDEO files', 'MP3 files', 'GIF files']
+
+def Scanner():
+    for loop in range(0,4):
+        if not os.path.exists(path_sorted + folder_names[loop]):
+            print(path_sorted + folder_names[loop])
+            os.makedirs((path_sorted + folder_names[loop]))
+            
+        
+
+def File_creation():
+    for file in file_name:
+        if ".mp3" in file and not os.path.exists(path_sorted + "/MP3 files/" +  file):
+            shutil.move(path_sorted + file, path_sorted + "/MP3 files/" +  file)
+        if ".png" in file and not os.path.exists(path_sorted + "/IMAGE files/" + file):
+            shutil.move(path_sorted + file, path_sorted + "/IMAGE files/" +  file)
+        if ".mp4" in file and not os.path.exists(path_sorted + "/VIDEO files/" + file):
+            shutil.move(path_sorted + file, path_sorted + "/VIDEO files/" +  file)
+        if ".webp" in file and not os.path.exists(path_sorted + "/VIDEO files/" + file):
+            shutil.move(path_sorted + file, path_sorted + "/VIDEO files/" +  file)
+        if ".gif" in file and not os.path.exists(path_sorted + "/GIF files/" + file):
+            shutil.move(path_sorted + file, path_sorted + "/GIF files/" +  file)
+        if ".jpeg" in file and not os.path.exists(path_sorted + "/IMAGE files/" + file):
+            shutil.move(path_sorted + file, path_sorted + "/IMAGE files/" +  file)
+        if ".jpg" in file and not os.path.exists(path_sorted + "/IMAGE files/" + file):
+            shutil.move(path_sorted + file, path_sorted + "/IMAGE files/" +  file)
 
 
-path = pathinput1 + '/'
 
-file_name = os.listdir(path)
-
-folder_names = ['IMAGE files', 'MP4 files', 'MP3 files', 'GIF files']
-
-for loop in range(0,4):
-    if not os.path.exists(path + folder_names[loop]):
-        print(path + folder_names[loop])
-        os.makedirs((path + folder_names[loop]))
-
-
-for file in file_name:
-    if ".mp3" in file and not os.path.exists(path + "/MP3 files/" +  file):
-        shutil.move(path + file, path + "/MP3 files/" +  file)
-    if ".png" in file and not os.path.exists(path + "/IMAGE files/" + file):
-        shutil.move(path + file, path + "/IMAGE files/" +  file)
-    if ".mp4" in file and not os.path.exists(path + "/MP4 files/" + file):
-        shutil.move(path + file, path + "/MP4 files/" +  file)
-    if ".gif" in file and not os.path.exists(path + "/GIF files/" + file):
-        shutil.move(path + file, path + "/GIF files/" +  file)
-    if ".jpeg" in file and not os.path.exists(path + "/IMAGE files/" + file):
-        shutil.move(path + file, path + "/IMAGE files/" +  file)
-    if ".jpg" in file and not os.path.exists(path + "/IMAGE files/" + file):
-        shutil.move(path + file, path + "/IMAGE files/" +  file)
-    
-
-else:
-    print("Some files didn't move")
+try:
+    print('Tc'''+'s Totally original file sorter')
+    print('Just place the address by copying and pasting it here')
+    print('NOTE: if youre going to sort an outside local disk, you should place "/" at the end, otherwise it wont work....')
+    Filedirectory = input('Input file directory: ')
+    #replace 'Filedirectory' to your prefered address here if you wanna do it manually(dont forget to include '\' in the end of the link)
+    path_sorted = Filedirectory 
+    file_name = os.listdir(path_sorted)
+    Scanner()
+    File_creation()
+except:
+    print('Error, Didnt sort the files.... please insert the appropriate syntax(like actually reading the NOTE)')
