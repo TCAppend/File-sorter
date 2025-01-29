5c500247b5066f2c2ff5d5eb58a1c0815f1fc143
TCJay_Append <123647533+TCBJYT@users.noreply.github.com>
2025-01-29 21:33:37 +0800

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,33 +1,47 @@
 import os, shutil
 
-folder_names = ['IMAGE files', 'VIDEO files', 'MP3 files', 'GIF files']
+folder_names = ['IMAGE files', 'VIDEO files', 'AUDIO files', 'GIF files', 'OFFICE files', 'PAINT.NET files', 'ADOBE files', 'EXECUTABLE files', 'COMPRESSED files']
 
 def Scanner():
-    for loop in range(0,4):
+    for loop in range(0,9):
         if not os.path.exists(path_sorted + folder_names[loop]):
             print(path_sorted + folder_names[loop])
             os.makedirs((path_sorted + folder_names[loop]))
             
         
-
+#careful, cringe 'if' statements
 def File_creation():
     for file in file_name:
-        if ".mp3" in file and not os.path.exists(path_sorted + "/MP3 files/" +  file):
-            shutil.move(path_sorted + file, path_sorted + "/MP3 files/" +  file)
+        if ".mp3" in file and not os.path.exists(path_sorted + "/AUDIO files/" +  file):
+            shutil.move(path_sorted + file, path_sorted + "/AUDIO files/" +  file)
         if ".png" in file and not os.path.exists(path_sorted + "/IMAGE files/" + file):
             shutil.move(path_sorted + file, path_sorted + "/IMAGE files/" +  file)
         if ".mp4" in file and not os.path.exists(path_sorted + "/VIDEO files/" + file):
             shutil.move(path_sorted + file, path_sorted + "/VIDEO files/" +  file)
-        if ".webp" in file and not os.path.exists(path_sorted + "/VIDEO files/" + file):
-            shutil.move(path_sorted + file, path_sorted + "/VIDEO files/" +  file)
+        if ".webp" in file and not os.path.exists(path_sorted + "/IMAGE files/" + file):
+            shutil.move(path_sorted + file, path_sorted + "/IMAGE files/" +  file)
         if ".gif" in file and not os.path.exists(path_sorted + "/GIF files/" + file):
             shutil.move(path_sorted + file, path_sorted + "/GIF files/" +  file)
         if ".jpeg" in file and not os.path.exists(path_sorted + "/IMAGE files/" + file):
             shutil.move(path_sorted + file, path_sorted + "/IMAGE files/" +  file)
         if ".jpg" in file and not os.path.exists(path_sorted + "/IMAGE files/" + file):
             shutil.move(path_sorted + file, path_sorted + "/IMAGE files/" +  file)
-
-
+        if file.endswith((".doc", ".docx", ".xls", ".xlsx", ".ppt", ".pptx", ".pdf")) and not os.path.exists(path_sorted + "/OFFICE files/" + file):
+            shutil.move(path_sorted + file, path_sorted + "/OFFICE files/" + file)
+        if ".pdn" in file and not os.path.exists(path_sorted + "/PAINT.NET files/" + file):
+            shutil.move(path_sorted + file, path_sorted + "/PAINT.NET files/" + file)
+        if file.endswith((".psd", ".ai", ".indd", ".xd")) and not os.path.exists(path_sorted + "/ADOBE files/" + file):
+            shutil.move(path_sorted + file, path_sorted + "/ADOBE files/" + file)
+        if ".exe" in file and not os.path.exists(path_sorted + "/EXECUTABLE files/" + file):
+            shutil.move(path_sorted + file, path_sorted + "/EXECUTABLE files/" + file)
+        if file.endswith((".zip", ".rar", ".7z", ".tar", ".gz")) and not os.path.exists(path_sorted + "/COMPRESSED files/" + file):
+            shutil.move(path_sorted + file, path_sorted + "/COMPRESSED files/" + file)
+        if ".txt" in file and not os.path.exists(path_sorted + "/TEXT files/" + file):
+            shutil.move(path_sorted + file, path_sorted + "/TEXT files/" + file)
+        if file.endswith((".wav", ".flac", ".aac")) and not os.path.exists(path_sorted + "/AUDIO files/" + file):
+            shutil.move(path_sorted + file, path_sorted + "/AUDIO files/" + file)
+        if file.endswith((".avi", ".mkv", ".mov")) and not os.path.exists(path_sorted + "/VIDEO files/" + file):
+            shutil.move(path_sorted + file, path_sorted + "/VIDEO files/" + file)    
 
 try:
     print('Tc'''+'s Totally original file sorter')
